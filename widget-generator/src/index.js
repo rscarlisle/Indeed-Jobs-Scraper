fe4bb8345aacfe074e8b9b926a63b6cfca71da6b
Randy Carlisle <cisolutionsinc@gmail.com>
2017-08-10 16:24:15 -0700

    update latest files

--- widget-generator/src/index.js
+++ widget-generator/src/index.js
@@ -1,7 +1,15 @@
+const OnlineJobBoardPageScraper = require('./scrapers/OnlineJobBoardPageScraper');
+// const scraper = new OnlineJobBoardPageScraper();
+// console.log(onlineboard.scrape("http://localhost:5000/sources/front-end-web-developer.htm"))
+
 addEventListener('DOMContentLoaded', main);
 
 function main() {
-  const $root = document.querySelector('#root');
-  const $app = null; // <== Your dynamically generated DOM element goes here
-  $root.appendChild($app);
+  const scraper = new OnlineJobBoardPageScraper();
+  scraper.scrape('http://localhost:5000/sources/front-end-web-developer.htm', '#root');
+
+  // const $root = document.querySelector('#root');
+  // const $app = null; // <== Your dynamically generated DOM element goes here
+  // $root.appendChild($app);
 }
+
---
+++ widget-generator/src/scrapers/OnlineJobBoardPageScraper.js
@@ -0,0 +1,63 @@
+const { div, p, h1, h2, ul, li } = require('elementx');
+
+module.exports = class OnlineJobBoardPageScraper {
+  constructor() { }
+
+  scrape(url, targetDiv='#root') {
+    fetch(url)
+      .then(response => {
+        return response.text();
+      })
+      .then(afterResponse => {
+        // Extract info from the page
+        const content = this.extract(afterResponse);
+        // console.log('hi')
+        // Create the HTML with elementx
+        const widget = this.buildHtml(content);
+
+        console.log('widget', widget);
+
+        const target = document.querySelector(targetDiv);
+        target.appendChild(widget);
+      })
+      .catch(err => err);
+  };
+
+  extract(html) {
+    // content extraction
+    const parser = new DOMParser();
+    const doc = parser.parseFromString(html, "text/html");
+    // returns a HTMLDocument, which also is a Document.
+
+    const pageTitle = doc.title;
+    // This is supposed to get the links, but it doesn't work perfectly
+    const jobtitles = doc.querySelectorAll('[data-tn-element]');
+
+    const contentObject = {
+      title: doc.title.split('|')[0],
+      description: 'A widget of jobs',
+      jobs: jobtitles
+    };
+console.log(contentObject)
+    return contentObject;
+  }
+
+  buildHtml(obj) {
+    // Create a list of job titles
+    const jobTitle = li(obj.innerText);
+    // obj.jobs.forEach(job => {
+    //   // Push elementx `<li>` elements into the `jobListItems`
+    //   jobListItems.push(li(job.innerText));
+  
+
+    // Build the HTML
+    const htmlData = div({ class: 'widget' },
+      h1(obj.title),
+      div(obj.description),
+      ul(
+        jobTitle // The list of job titles
+      )
+    );
+    return(htmlData);
+    }
+  }
