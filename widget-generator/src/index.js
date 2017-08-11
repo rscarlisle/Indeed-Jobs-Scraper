fa85cc3007580102b574e523a26a3a77d8b84cfc
Randy Carlisle <cisolutionsinc@gmail.com>
2017-08-11 09:32:22 -0700

    update latest changes

--- widget-generator/src/index.js
+++ widget-generator/src/index.js
@@ -2,14 +2,28 @@ const OnlineJobBoardPageScraper = require('./scrapers/OnlineJobBoardPageScraper'
 // const scraper = new OnlineJobBoardPageScraper();
 // console.log(onlineboard.scrape("http://localhost:5000/sources/front-end-web-developer.htm"))
 
-addEventListener('DOMContentLoaded', main);
+  addEventListener('DOMContentLoaded', main);
 
 function main() {
-  const scraper = new OnlineJobBoardPageScraper();
-  scraper.scrape('http://localhost:5000/sources/front-end-web-developer.htm', '#root');
 
+  document.getElementById("myBtn").addEventListener("click", function(e){
+      console.log('here!!')
+      e.preventDefault();
+      const passUrl = document.getElementById("myUrl").value;
+      console.log(passUrl)
+      const scraper = new OnlineJobBoardPageScraper();
+      scraper.scrape(passUrl, '#root');
+  });
+
+  // const scraper = new OnlineJobBoardPageScraper();
+  // scraper.scrape('http://localhost:5000/sources/front-end-web-developer.htm', '#root');
+
+  // scraper.scrape('http://localhost:5000/sources/junior-front-end-web-developer.htm', '#root');
+  
+  // scraper.scrape('http://localhost:5000/sources/junior-front-end-web-developer.htm', '#root');
+  
   // const $root = document.querySelector('#root');
-  // const $app = null; // <== Your dynamically generated DOM element goes here
+  // const $app = null; // <== dynamically generated DOM element goes here
   // $root.appendChild($app);
 }
 
